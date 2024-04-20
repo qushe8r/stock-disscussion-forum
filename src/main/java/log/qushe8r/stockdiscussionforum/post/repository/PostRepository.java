ae3b83e6e096105a9e84060b5f54b27bcd30629e
qushe8r <qushe8r@gmail.com>
2024-04-20 17:07:08 +0900

    feat: PostRepository 추가 #3

---
+++ src/main/java/log/qushe8r/stockdiscussionforum/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package log.qushe8r.stockdiscussionforum.post.repository;
+
+import log.qushe8r.stockdiscussionforum.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
