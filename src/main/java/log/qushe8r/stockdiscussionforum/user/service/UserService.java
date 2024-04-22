06c8a33d137b47e641c4d2bc23f085a3c967f88e
qushe8r <qushe8r@gmail.com>
2024-04-22 17:13:38 +0900

    rename: UserService 메소드명 변경 #2

--- src/main/java/log/qushe8r/stockdiscussionforum/user/service/UserService.java
+++ src/main/java/log/qushe8r/stockdiscussionforum/user/service/UserService.java
@@ -32,9 +32,11 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void create(UserCreateRequest request) {
+    public void registerUser(UserCreateRequest request) {
         userRepository.findByUsername(request.username())
-                .ifPresent(user -> { throw new UserException(UserExceptionCode.USER_ALREADY_EXIST); });
+                .ifPresent(user -> {
+                    throw new UserException(UserExceptionCode.USER_ALREADY_EXIST);
+                });
 
         String encodedPassword = passwordEncoder.encode(request.password());
         User user = userMapper.toEntity(request, encodedPassword);
@@ -47,13 +49,13 @@ public class UserService {
 
 
     @Transactional
-    public void modify(Long userId, UserModifyRequest request) {
+    public void modifyInformation(Long userId, UserModifyRequest request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
-        user.modify(request.nickname(), request.bio());
+        user.modifyInformation(request.nickname(), request.bio());
     }
 
-    public UserDetailsResponse get(Long userId) {
+    public UserDetailsResponse findById(Long userId) {
         return userRepository.findById(userId)
                 .map(userMapper::toDetailsResponse)
                 .orElseThrow();
@@ -64,7 +66,7 @@ public class UserService {
                 .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
     }
 
-    public List<UserResponse> getAll() {
+    public List<UserResponse> findAll() {
         // TODO: 페이징 처리 ? 유저 리스트가 필요하긴 함...?
         return userRepository.findAll()
                 .stream().map(userMapper::toResponse)
@@ -72,7 +74,7 @@ public class UserService {
     }
 
     @Transactional
-    public void delete(Long userId) {
+    public void deleteById(Long userId) {
         userRepository.deleteById(userId);
     }
 
@@ -83,8 +85,8 @@ public class UserService {
             return "인증 코드의 유효 시간은 10분입니다.";
         }
         if (code.equals(savedCode)) {
-            updateUserStatus(userId, UserStatus.ACTIVE);
-            updateUserRole(userId, UserRole.ROLE_USER);
+            modifyUserStatus(userId, UserStatus.ACTIVE);
+            modifyUserRole(userId, UserRole.ROLE_USER);
             verificationCodeService.removeVerificationCode(userId);
             return "이메일이 성공적으로 인증되었습니다.";
         }
@@ -96,17 +98,17 @@ public class UserService {
     }
 
 
-    private void updateUserStatus(Long userId, UserStatus userStatus) {
+    private void modifyUserStatus(Long userId, UserStatus userStatus) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
-        user.updateStatus(userStatus);
+        user.modifyStatus(userStatus);
         userRepository.save(user);
     }
 
-    private void updateUserRole(Long userId, UserRole userRole) {
+    private void modifyUserRole(Long userId, UserRole userRole) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserExceptionCode.USER_NOT_FOUND));
-        user.updateRole(userRole);
+        user.modifyRole(userRole);
         userRepository.save(user);
     }
 
