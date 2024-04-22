8fe9b70c747983b5de794e8b09a0feab9cef9f43
qushe8r <qushe8r@gmail.com>
2024-04-22 17:16:38 +0900

    feat: UserController 비밀번호 변경 추가 #2

--- src/main/java/log/qushe8r/stockdiscussionforum/user/controller/UserController.java
+++ src/main/java/log/qushe8r/stockdiscussionforum/user/controller/UserController.java
@@ -1,12 +1,11 @@
 package log.qushe8r.stockdiscussionforum.user.controller;
 
-import log.qushe8r.stockdiscussionforum.user.dto.UserCreateRequest;
-import log.qushe8r.stockdiscussionforum.user.dto.UserDetailsResponse;
-import log.qushe8r.stockdiscussionforum.user.dto.UserModifyRequest;
-import log.qushe8r.stockdiscussionforum.user.dto.UserResponse;
+import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
+import log.qushe8r.stockdiscussionforum.user.dto.*;
 import log.qushe8r.stockdiscussionforum.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +33,15 @@ public class UserController {
         userService.modifyInformation(userId, request);
         return ResponseEntity.ok().build();
     }
+
+    @PatchMapping("/users/{userId}/password")
+    public ResponseEntity<Void> modifyUserPassword(@PathVariable Long userId,
+                                                   @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+                                                   @RequestBody PasswordModifyRequest request) {
+        userService.modifyPassword(userId, authenticatedUser, request);
+        return ResponseEntity.ok().build();
+    }
+
 
     @GetMapping("/users/all")
     public ResponseEntity<List<UserResponse>> findAll() {
