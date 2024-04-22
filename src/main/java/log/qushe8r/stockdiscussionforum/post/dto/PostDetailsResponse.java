0bc8800a5e2cf9c609f5ff331b39e38f703114ed
qushe8r <qushe8r@gmail.com>
2024-04-22 17:39:28 +0900

    feat: PostDetailsResponse 댓글 응답 추가 #5

--- src/main/java/log/qushe8r/stockdiscussionforum/post/dto/PostDetailsResponse.java
+++ src/main/java/log/qushe8r/stockdiscussionforum/post/dto/PostDetailsResponse.java
@@ -1,5 +1,9 @@
 package log.qushe8r.stockdiscussionforum.post.dto;
 
-public record PostDetailsResponse(Long postId, String title, String content) {
+import log.qushe8r.stockdiscussionforum.comment.dto.CommentResponse;
+
+import java.util.List;
+
+public record PostDetailsResponse(Long postId, String title, String content, List<CommentResponse> comments) {
 
 }
