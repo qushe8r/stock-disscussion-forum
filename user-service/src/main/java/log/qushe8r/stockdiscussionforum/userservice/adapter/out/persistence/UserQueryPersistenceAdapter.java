be224c238ece4dd022abcde41df27f94edd140d3
qushe8r <qushe8r@gmail.com>
2024-04-25 09:22:52 +0900

    feat: userservice: UserQueryPersistencePort 구현 #28

---
+++ user-service/src/main/java/log/qushe8r/stockdiscussionforum/userservice/adapter/out/persistence/UserQueryPersistenceAdapter.java
@@ -0,0 +1,25 @@
+package log.qushe8r.stockdiscussionforum.userservice.adapter.out.persistence;
+
+import log.qushe8r.stockdiscussionforum.common.PersistenceAdapter;
+import log.qushe8r.stockdiscussionforum.userservice.application.port.out.UserQueryPersistencePort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class UserQueryPersistenceAdapter implements UserQueryPersistencePort {
+
+    private final UserJpaRepository userJpaRepository;
+
+    @Override
+    public Optional<UserJpaEntity> findByUsername(String username) {
+        return userJpaRepository.findByUsername(username);
+    }
+
+    @Override
+    public Optional<UserJpaEntity> findById(Long userId) {
+        return userJpaRepository.findById(userId);
+    }
+
+}
