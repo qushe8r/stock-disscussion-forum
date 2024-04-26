55e5beafec200a30bf93f7daa417e51c2f343d58
qushe8r <qushe8r@gmail.com>
2024-04-26 17:57:38 +0900

    feat: activityservice: PostMapper -> toDomainEntity, toJpaEntity 구현 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/application/service/PostMapper.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/application/service/PostMapper.java
@@ -2,6 +2,8 @@ package log.qushe8r.stockdiscussionforum.activityservice.application.service;
 
 import log.qushe8r.stockdiscussionforum.activityservice.adapter.out.PostJpaEntity;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostRegistrationCommand;
+import log.qushe8r.stockdiscussionforum.activityservice.domain.Post;
+import log.qushe8r.stockdiscussionforum.activityservice.domain.Writer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +13,24 @@ public class PostMapper {
         String content = postRegistrationCommand.getContent();
 
         return new PostJpaEntity(title, content, writerId);
+    }
+
+    public Post toDomainEntity(PostJpaEntity postJpaEntity) {
+        Long postId = postJpaEntity.getId();
+        String title = postJpaEntity.getTitle();
+        String content = postJpaEntity.getContent();
+        Long writerId = postJpaEntity.getWriterId();
+        Writer writer = new Writer(writerId, null);
+
+        return Post.create(postId, title, content, writer, null);
+    }
+
+    public PostJpaEntity toJpaEntity(Post post) {
+        return new PostJpaEntity(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getWriter().id()
+        );
     }
 }
