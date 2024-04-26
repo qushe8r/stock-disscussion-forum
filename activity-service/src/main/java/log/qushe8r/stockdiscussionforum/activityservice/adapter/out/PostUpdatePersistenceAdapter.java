be044eb61c9049047ee90355fa02a9ab98735945
qushe8r <qushe8r@gmail.com>
2024-04-26 17:57:16 +0900

    feat: activityservice: PostUpdatePersistencePort -> updatePost 구현 #28

---
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/PostUpdatePersistenceAdapter.java
@@ -0,0 +1,18 @@
+package log.qushe8r.stockdiscussionforum.activityservice.adapter.out;
+
+import log.qushe8r.stockdiscussionforum.activityservice.application.port.out.PostUpdatePersistencePort;
+import log.qushe8r.stockdiscussionforum.common.PersistenceAdapter;
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class PostUpdatePersistenceAdapter implements PostUpdatePersistencePort {
+
+    private final PostJpaRepository repository;
+
+    @Override
+    public void updatePost(PostJpaEntity postJpaEntity) {
+        repository.save(postJpaEntity);
+    }
+
+}
