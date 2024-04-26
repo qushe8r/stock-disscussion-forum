b18d26ddc01533a557e4a50f53971f383d27b52b
qushe8r <qushe8r@gmail.com>
2024-04-26 17:57:55 +0900

    feat: activityservice: PostJpaEntity -> @Getter 추가 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/PostJpaEntity.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/PostJpaEntity.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
