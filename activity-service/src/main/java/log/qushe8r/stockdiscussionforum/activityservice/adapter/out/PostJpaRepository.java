ed7a2e4db545f1dd930f8cede875532287f4c448
qushe8r <qushe8r@gmail.com>
2024-04-26 17:33:29 +0900

    feat: activityservice: PostJpaRepository 추가 #28

---
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/PostJpaRepository.java
@@ -0,0 +1,6 @@
+package log.qushe8r.stockdiscussionforum.activityservice.adapter.out;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostJpaRepository extends JpaRepository<PostJpaEntity, Long> {
+}
