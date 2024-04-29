e465616e0fbc6814d47818900b6f438e9340d338
qushe8r <qushe8r@gmail.com>
2024-04-29 16:11:28 +0900

    feat: activity-service: @EnableFeignClients 추가 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/ActivityServiceApplication.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/ActivityServiceApplication.java
@@ -2,8 +2,10 @@ package log.qushe8r.stockdiscussionforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 public class ActivityServiceApplication {
