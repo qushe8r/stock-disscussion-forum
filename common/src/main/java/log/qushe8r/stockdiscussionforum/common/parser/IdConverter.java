1c7bbe5f5df50ee5f4b2171ea829f94fb42e61c8
qushe8r <qushe8r@gmail.com>
2024-04-29 16:34:11 +0900

    feat: common: IdConverter List<Long> -> String 추가, String -> List<Long> 변경 #28

--- common/src/main/java/log/qushe8r/stockdiscussionforum/common/parser/IdConverter.java
+++ common/src/main/java/log/qushe8r/stockdiscussionforum/common/parser/IdConverter.java
@@ -3,17 +3,21 @@ package log.qushe8r.stockdiscussionforum.common.parser;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class IdConverter {
     public static List<Long> parser(String userIds) {
-        List<Long> userIdsList = new ArrayList<>();
-        String[] userIdsArray = userIds.split(",");
-        for (String userId : userIdsArray) {
-            userIdsList.add(Long.parseLong(userId.trim()));
-        }
-        return userIdsList;
+        return Arrays.stream(userIds.split(","))
+                .map(String::trim)
+                .map(Long::parseLong)
+                .toList();
+    }
+
+    public static String parser(List<Long> ids) {
+        return ids.stream().map(Object::toString)
+                .collect(Collectors.joining(","));
     }
 }
