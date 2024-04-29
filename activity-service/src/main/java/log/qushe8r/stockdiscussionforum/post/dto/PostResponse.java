113ede9e941ca619cd7eaf38e0f896041007e741
qushe8r <qushe8r@gmail.com>
2024-04-29 10:31:42 +0900

    remove: activityservice: 이전 클래스 삭제 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/post/dto/PostResponse.java
+++
@@ -1,4 +0,0 @@
-package log.qushe8r.stockdiscussionforum.post.dto;
-
-public record PostResponse(Long postId, String title, String content, String writer) {
-}
--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/post/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package log.qushe8r.stockdiscussionforum.post.repository;
-
-import log.qushe8r.stockdiscussionforum.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-    void deleteByWriterIdAndId(Long writerId, Long postId);
-}
