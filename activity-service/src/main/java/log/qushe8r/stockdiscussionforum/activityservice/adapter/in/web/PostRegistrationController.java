17dc3356b6763644f78cb5bfc896ed80a76a98df
qushe8r <qushe8r@gmail.com>
2024-04-29 16:36:30 +0900

    feat: activity-service: AuthenticatedUser -> Long 파라미터 변경 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostRegistrationController.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostRegistrationController.java
@@ -3,14 +3,9 @@ package log.qushe8r.stockdiscussionforum.activityservice.adapter.in.web;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostRegistrationCommand;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostRegistrationUseCase;
 import log.qushe8r.stockdiscussionforum.common.WebAdapter;
-import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @WebAdapter
 @RestController
@@ -21,10 +16,8 @@ public class PostRegistrationController {
     private final PostRegistrationUseCase useCase;
 
     @PostMapping
-    public ResponseEntity<Void> d(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
-                                  @RequestBody PostRegistrationCommand command) {
-        Long userId = authenticatedUser.getUserId();
-
+    public ResponseEntity<Void> registerPost(@RequestHeader Long userId,
+                                             @RequestBody PostRegistrationCommand command) {
         useCase.registerPost(userId, command);
         return ResponseEntity.ok().build();
     }
