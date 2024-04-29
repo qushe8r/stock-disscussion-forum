f8a509fd36bdc92cb2c2760e05473aea4981b9a6
qushe8r <qushe8r@gmail.com>
2024-04-29 20:33:03 +0900

    feat: activity-service: CommentDeletion 인터페이스 정의 #28

---
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/comment/persistence/CommentDeletionPersistencePort.java
@@ -0,0 +1,7 @@
+package log.qushe8r.stockdiscussionforum.activityservice.adapter.out.comment.persistence;
+
+public interface CommentDeletionPersistencePort {
+
+    void deleteByCommentId(Long commentId);
+
+}
