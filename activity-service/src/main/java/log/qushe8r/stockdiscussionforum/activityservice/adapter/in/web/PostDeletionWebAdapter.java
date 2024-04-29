9ad5f0dfe74b734f1f00bb49736f7afd23dea2d9
qushe8r <qushe8r@gmail.com>
2024-04-29 16:43:28 +0900

    refactor: activity-service: PostDeletionWebAdapter AuthenticatedUser -> Long 파라미터 변경 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostDeletionWebAdapter.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostDeletionWebAdapter.java
@@ -3,14 +3,9 @@ package log.qushe8r.stockdiscussionforum.activityservice.adapter.in.web;
 
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostDeletionUseCase;
 import log.qushe8r.stockdiscussionforum.common.WebAdapter;
-import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @WebAdapter
 @RestController
@@ -21,10 +16,8 @@ public class PostDeletionWebAdapter {
     private final PostDeletionUseCase useCase;
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+    public ResponseEntity<Void> deletePost(@RequestHeader Long userId,
                                            @PathVariable Long postId) {
-        Long userId = authenticatedUser.getUserId();
-
         useCase.deletePost(userId, postId);
         return ResponseEntity.ok().build();
     }
