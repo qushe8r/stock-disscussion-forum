21ff30c35772a0ddc9d8115db3859c1b1fd5df6e
qushe8r <qushe8r@gmail.com>
2024-04-29 16:23:25 +0900

    feat: user-service: controller AuthenticatedUser -> Long 파라미터 변경 #28

--- user-service/src/main/java/log/qushe8r/stockdiscussionforum/userservice/adapter/in/web/UserDeletionController.java
+++ user-service/src/main/java/log/qushe8r/stockdiscussionforum/userservice/adapter/in/web/UserDeletionController.java
@@ -1,13 +1,12 @@
 package log.qushe8r.stockdiscussionforum.userservice.adapter.in.web;
 
 import log.qushe8r.stockdiscussionforum.common.WebAdapter;
-import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
 import log.qushe8r.stockdiscussionforum.userservice.application.port.in.UserDeletionUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @WebAdapter
@@ -18,10 +17,8 @@ public class UserDeletionController {
     private final UserDeletionUseCase useCase;
 
     @DeleteMapping(path = "/{requestedUserId}")
-    public ResponseEntity<Void> deleteById(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+    public ResponseEntity<Void> deleteById(@RequestHeader(name = "userId") Long requestingUserId,
                                            @PathVariable Long requestedUserId) {
-        Long requestingUserId = authenticatedUser.getUserId();
-
         useCase.deleteUser(requestingUserId, requestedUserId);
         return ResponseEntity.ok().build();
     }
--- user-service/src/main/java/log/qushe8r/stockdiscussionforum/userservice/adapter/in/web/UserUpdateController.java
+++ user-service/src/main/java/log/qushe8r/stockdiscussionforum/userservice/adapter/in/web/UserUpdateController.java
@@ -1,18 +1,15 @@
 package log.qushe8r.stockdiscussionforum.userservice.adapter.in.web;
 
 import log.qushe8r.stockdiscussionforum.common.WebAdapter;
-import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
 import log.qushe8r.stockdiscussionforum.userservice.application.port.in.UserPasswordUpdateCommand;
 import log.qushe8r.stockdiscussionforum.userservice.application.port.in.UserUpdateCommand;
 import log.qushe8r.stockdiscussionforum.userservice.application.port.in.UserUpdateUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @WebAdapter
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserUpdateController {
@@ -20,19 +17,15 @@ public class UserUpdateController {
     private final UserUpdateUseCase useCase;
 
     @PatchMapping("/me")
-    public ResponseEntity<Void> userUpdateInformation(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+    public ResponseEntity<Void> userUpdateInformation(@RequestHeader Long userId,
                                                       @RequestBody UserUpdateCommand command) {
-        Long userId = authenticatedUser.getUserId();
-
         useCase.updateUserInformation(userId, command);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/me/password")
-    public ResponseEntity<Void> userUpdatePassword(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+    public ResponseEntity<Void> userUpdatePassword(@RequestHeader Long userId,
                                                    @RequestBody UserPasswordUpdateCommand command) {
-        Long userId = authenticatedUser.getUserId();
-
         useCase.updateUserPassword(userId, command);
         return ResponseEntity.ok().build();
     }
