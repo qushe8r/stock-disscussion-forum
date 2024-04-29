7ad68fa10799fb8e6727fd6654c28abdf4cf60fe
qushe8r <qushe8r@gmail.com>
2024-04-29 16:36:58 +0900

    feat: activity-service: AuthenticatedUser -> Long 파라미터 변경 #28

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostUpdateController.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/in/web/PostUpdateController.java
@@ -3,26 +3,22 @@ package log.qushe8r.stockdiscussionforum.activityservice.adapter.in.web;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostUpdateCommand;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.PostUpdateUseCase;
 import log.qushe8r.stockdiscussionforum.common.WebAdapter;
-import log.qushe8r.stockdiscussionforum.security.user.AuthenticatedUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/posts")
 public class PostUpdateController {
 
     private final PostUpdateUseCase useCase;
 
     @PatchMapping("/{postId}")
-    public ResponseEntity<Void> updatePost(@AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+    public ResponseEntity<Void> updatePost(@RequestHeader Long userId,
                                            @PathVariable Long postId,
                                            @RequestBody PostUpdateCommand command) {
-        Long userId = authenticatedUser.getUserId();
-
         useCase.updatePost(userId, postId, command);
         return ResponseEntity.ok().build();
     }
