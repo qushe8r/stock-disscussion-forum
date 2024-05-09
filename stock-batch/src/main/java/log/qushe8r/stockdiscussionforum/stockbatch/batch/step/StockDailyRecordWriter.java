25075a662415dff3894497c477da89173025ba4f
qushe8r <qushe8r@gmail.com>
2024-05-09 19:55:35 +0900

    feat: stock-batch: StockDailyRecordWriter 추가 #31

---
+++ stock-batch/src/main/java/log/qushe8r/stockdiscussionforum/stockbatch/batch/step/StockDailyRecordWriter.java
@@ -0,0 +1,25 @@
+package log.qushe8r.stockdiscussionforum.stockbatch.batch.step;
+
+import log.qushe8r.stockdiscussionforum.stockbatch.stock.entity.StockDailyRecord;
+import log.qushe8r.stockdiscussionforum.stockbatch.stock.repository.StockDailyRecordRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StockDailyRecordWriter implements ItemWriter<List<StockDailyRecord>> {
+
+    private final StockDailyRecordRepository repository;
+
+    @Override
+    public void write(Chunk<? extends List<StockDailyRecord>> chunk) {
+        chunk.getItems().forEach(repository::saveAll);
+    }
+
+}
