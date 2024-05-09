fae64774e6a56eea1f774c37cf29d6121b100561
qushe8r <qushe8r@gmail.com>
2024-05-09 22:24:32 +0900

    feat: stock-service: stock-service 모듈 생성 #30

---
+++ stock-service/src/main/java/log/qushe8r/stockdiscussionforum/StockServiceApplication.java
@@ -0,0 +1,14 @@
+package log.qushe8r.stockdiscussionforum;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockServiceApplication.class, args);
+    }
+
+}
