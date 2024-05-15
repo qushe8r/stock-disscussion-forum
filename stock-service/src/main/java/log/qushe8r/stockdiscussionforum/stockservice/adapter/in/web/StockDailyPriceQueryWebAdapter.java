82a7c0775ca1557bb6819f5ccba5989fdf73b2b0
qushe8r <qushe8r@gmail.com>
2024-05-16 01:18:52 +0900

    feat: stock-service: StockDailyPriceQueryWebAdapter 추가 #30

---
+++ stock-service/src/main/java/log/qushe8r/stockdiscussionforum/stockservice/adapter/in/web/StockDailyPriceQueryWebAdapter.java
@@ -0,0 +1,29 @@
+package log.qushe8r.stockdiscussionforum.stockservice.adapter.in.web;
+
+import log.qushe8r.stockdiscussionforum.common.WebAdapter;
+import log.qushe8r.stockdiscussionforum.stockservice.application.port.in.StockDailyPriceQueryUseCase;
+import log.qushe8r.stockdiscussionforum.stockservice.application.port.in.StockDailyPriceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stocks")
+public class StockDailyPriceQueryWebAdapter {
+
+    private final StockDailyPriceQueryUseCase useCase;
+
+    @GetMapping("/{itemCode}")
+    public ResponseEntity<StockDailyPriceResponse> stockDailyPriceQuery(@PathVariable String itemCode,
+                                                                        Pageable pageable) {
+        useCase.stockDailyPrice(itemCode, pageable);
+        return ResponseEntity.ok(new StockDailyPriceResponse());
+    }
+
+}
