dd5763dc6307907a1ff8ac2a46318362729d7c06
qushe8r <qushe8r@gmail.com>
2024-06-19 13:35:42 +0900

    refactor: activity-service: Post JPA 정리 #37

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/post/persistence/PostJpaEntity.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/post/persistence/PostJpaEntity.java
@@ -2,6 +2,7 @@ package log.qushe8r.stockdiscussionforum.activityservice.adapter.out.post.persis
 
 import jakarta.persistence.*;
 import log.qushe8r.stockdiscussionforum.activityservice.adapter.out.comment.persistence.CommentJpaEntity;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,19 +12,26 @@ import java.util.List;
 
 @Getter
 @Entity
+@Table(name = "as_posts")
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostJpaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id", unique = true, nullable = false, updatable = false)
     private Long id;
 
+    @Column(name = "title", nullable = false, length = 100)
     private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @Column(name = "post_writer_id", nullable = false, updatable = false)
     private Long writerId;
 
-    @OneToMany(mappedBy = "postJpaEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "postJpaEntity", orphanRemoval = true)
     private List<CommentJpaEntity> commentJpaEntities = new ArrayList<>();
 
     public PostJpaEntity(Long id) {
--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/post/persistence/PostLikeJpaEntity.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/post/persistence/PostLikeJpaEntity.java
@@ -1,26 +1,28 @@
 package log.qushe8r.stockdiscussionforum.activityservice.adapter.out.post.persistence;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "post_like")
+@Table(name = "as_post_likes")
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostLikeJpaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id", unique = true, nullable = false, updatable = false)
     private Long id;
 
-    @Column(name = "userId")
+    @Column(name = "user_id", nullable = false, updatable = false)
     private Long userId;
 
-    @ManyToOne
-    @JoinColumn(name = "post_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false, updatable = false)
     private PostJpaEntity postJpaEntity;
 
     public PostLikeJpaEntity(Long userId, PostJpaEntity postJpaEntity) {
