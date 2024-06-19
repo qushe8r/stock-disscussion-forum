e9f0ec228c54c7a2d754b6b0496175c142d3b0c6
qushe8r <qushe8r@gmail.com>
2024-06-19 13:51:08 +0900

    refactor: newsfeed-service: Follow JPA 정리 #37

--- newsfeed-service/src/main/java/log/qushe8r/stockdiscussionforum/newfeedservice/adapter/out/persistence/FollowJpaEntity.java
+++ newsfeed-service/src/main/java/log/qushe8r/stockdiscussionforum/newfeedservice/adapter/out/persistence/FollowJpaEntity.java
@@ -7,16 +7,20 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "follows")
+@Table(name = "nfs_follows")
 @NoArgsConstructor
 @AllArgsConstructor
 public class FollowJpaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id", unique = true, nullable = false, updatable = false)
     private Long id;
 
+    @Column(name = "follower_id", nullable = false, updatable = false)
     private Long followerId;
+
+    @Column(name = "followee_id", nullable = false, updatable = false)
     private Long followeeId;
 
     public FollowJpaEntity(Long followerId, Long followeeId) {
