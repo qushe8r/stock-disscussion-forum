b0c427e5c6fc51647095c07dd4f749b392afe00c
qushe8r <qushe8r@gmail.com>
2024-06-24 18:17:20 +0900

    rename: activity-service: CommentLike commentLikeCount rename #37

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/domain/CommentLike.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/domain/CommentLike.java
@@ -1,4 +1,4 @@
 package log.qushe8r.stockdiscussionforum.activityservice.domain;
 
-public record CommentLike(Long postLikeCount, boolean likeByRequestingUser) {
+public record CommentLike(Long commentLikeCount, boolean likeByRequestingUser) {
 }
