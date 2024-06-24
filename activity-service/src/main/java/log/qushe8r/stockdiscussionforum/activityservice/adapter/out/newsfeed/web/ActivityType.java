1848687f0487e216ac2179a3d67fa217432b38a5
qushe8r <qushe8r@gmail.com>
2024-06-24 18:21:28 +0900

    refactor: activity-service: ResourceType 추가, ActivityType 변경 #37

---
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/newsfeed/web/ActivityType.java
@@ -0,0 +1,5 @@
+package log.qushe8r.stockdiscussionforum.activityservice.adapter.out.newsfeed.web;
+
+public enum ActivityType {
+	POST, UPDATE, DELETE
+}
---
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/adapter/out/newsfeed/web/ResourceType.java
@@ -0,0 +1,5 @@
+package log.qushe8r.stockdiscussionforum.activityservice.adapter.out.newsfeed.web;
+
+public enum ResourceType {
+	FOLLOW, POST, POST_LIKE, COMMENT, COMMENT_LIKE
+}
