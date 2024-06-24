ae2cad5dbd038ce98321f0db7f3068e0962edf88
qushe8r <qushe8r@gmail.com>
2024-06-24 18:11:44 +0900

    refactor: activity-service: PostUpdateService -> NewsFeed 요청 추가 #37

--- activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/application/service/post/PostUpdateService.java
+++ activity-service/src/main/java/log/qushe8r/stockdiscussionforum/activityservice/application/service/post/PostUpdateService.java
@@ -1,5 +1,7 @@
 package log.qushe8r.stockdiscussionforum.activityservice.application.service.post;
 
+import log.qushe8r.stockdiscussionforum.activityservice.adapter.out.newsfeed.web.NewsFeedCommand;
+import log.qushe8r.stockdiscussionforum.activityservice.adapter.out.newsfeed.web.NewsFeedServiceClient;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.post.PostUpdateCommand;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.in.post.PostUpdateUseCase;
 import log.qushe8r.stockdiscussionforum.activityservice.application.port.out.post.persistence.PostQueryPersistencePort;
@@ -9,31 +11,34 @@ import log.qushe8r.stockdiscussionforum.activityservice.domain.exception.PostExc
 import log.qushe8r.stockdiscussionforum.activityservice.domain.exception.PostExceptionCode;
 import log.qushe8r.stockdiscussionforum.common.UseCase;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.transaction.annotation.Transactional;
 
 @UseCase
 @RequiredArgsConstructor
 public class PostUpdateService implements PostUpdateUseCase {
 
-    private final PostMapper mapper;
-    private final PostQueryPersistencePort queryPort;
-    private final PostUpdatePersistencePort persistencePort;
+	private final PostMapper mapper;
+	private final PostQueryPersistencePort queryPort;
+	private final PostUpdatePersistencePort persistencePort;
+	private final NewsFeedServiceClient client;
 
-    @Transactional
-    @Override
-    public void updatePost(Long requestingUserId, Long postId, PostUpdateCommand command) {
-        String newTitle = command.getTitle();
-        String newContent = command.getContent();
+	@Transactional
+	@Override
+	public void updatePost(Long requestingUserId, Long postId, PostUpdateCommand command) {
+		String newTitle = command.getTitle();
+		String newContent = command.getContent();
 
-        Post post = queryPort.findById(postId)
-                .map(mapper::toDomainEntityWriterNicknameNull)
-                .orElseThrow(() -> new PostException(PostExceptionCode.POST_NOT_FOUND));
+		Post post = queryPort.findById(postId)
+			.map(mapper::toDomainEntityWriterNicknameNull)
+			.orElseThrow(() -> new PostException(PostExceptionCode.POST_NOT_FOUND));
 
-        post.update(requestingUserId, newTitle, newContent, this::updateExecute);
-    }
+		post.update(requestingUserId, newTitle, newContent, this::updateExecute);
+		client.registerNewsfeeds(NewsFeedCommand.postUpdate(requestingUserId, post.getId()));
+	}
 
-    private void updateExecute(Post post) {
-        persistencePort.updatePost(mapper.toJpaEntity(post));
-    }
+	private void updateExecute(Post post) {
+		persistencePort.updatePost(mapper.toJpaEntity(post));
+	}
 
 }
